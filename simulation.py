329e5407df13336938839357428cdae7e6c93100
David Cairuz <davidcairuz@gmail.com>
2019-07-07 15:33:51 -0300

    improving code quality

--- simulation.py
+++ simulation.py
@@ -1,23 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 from tkinter import Canvas, Tk
 import time
-import numpy as np 
+import numpy as np
 
 WIDTH = 500
 HEIGHT = 400
+IND_SIZE = 10
 GENERATION_GAP = 200
 
 class Individual:
 
     all_individuals = {}
     current_generation = 0
-    
+
     def __init__(self):
-        self.shape = canvas.create_oval(10, 10, 20, 20, fill='red')
+        (x1, y1, x2, y2) = self.get_random_coordinates()
+        self.shape = canvas.create_oval(x1, y1, x2, y2, fill='red')
         self.xspeed = 1
         self.yspeed = 1
-        
+
+        self.change_direction(low=-1, high=1, chance=100)
         Individual.all_individuals[self.shape] = self
-            
+
+    def get_random_coordinates(self):
+        x1 = np.random.randint(0, WIDTH - IND_SIZE)
+        y1 = np.random.randint(0, HEIGHT - IND_SIZE)
+        x2 = x1 + IND_SIZE
+        y2 = y1 + IND_SIZE
+
+        return (x1, y1, x2, y2)
+
     def walk(self):
         if self.is_dead():
             return
@@ -26,21 +39,20 @@ class Individual:
         canvas.move(self.shape, self.xspeed, self.yspeed)
 
         self.kill_by_contact(starting_gen=GENERATION_GAP)
-        self.change_direction(low=-1, high=1, chance=95)
-        
+        self.change_direction(low=-1, high=1, chance=5)
 
     def is_dead(self):
         return self.shape not in Individual.all_individuals
-	
+
     def check_if_reached_border(self):
         pos = canvas.coords(self.shape)
-        
+
         if pos[3] >= HEIGHT or pos[1] <= 0:
             self.yspeed = -self.yspeed
 
         if pos[2] >= WIDTH or pos[0] <= 0:
             self.xspeed = -self.xspeed
-    
+
     def kill_by_contact(self, starting_gen):
         if Individual.current_generation < starting_gen:
             return
@@ -48,43 +60,43 @@ class Individual:
         currently_overlapping = self.detect_collision()
 
         for individual in currently_overlapping:
-            canvas.delete(individual) 
+            canvas.delete(individual)
             try:
                 del Individual.all_individuals[individual]
             except KeyError:
                 pass
-    
+
     def detect_collision(self):
-        x1, y1, x2, y2 = canvas.coords(self.shape)
+        (x1, y1, x2, y2) = canvas.coords(self.shape)
         currently_overlapping = canvas.find_overlapping(x1, y1, x2, y2)
-        
+
         if len(currently_overlapping) == 1:
             currently_overlapping = {}
 
         return currently_overlapping
-    
+
     def change_direction(self, low, high, chance):
-        # 95% chance of keep going in the same direction
-        if np.random.randint(0, 101) > chance:
-            self.xspeed = np.random.randint(low, high+1)
-            self.yspeed = np.random.randint(low, high+1)
+        if np.random.randint(0, 101) > 100 - chance:
+            self.xspeed = np.random.randint(low, high + 1)
+            self.yspeed = np.random.randint(low, high + 1)
+
 
 master = Tk()
 canvas = Canvas(master, width=WIDTH, height=HEIGHT)
-master.title("Simulation")
+master.title('Simulation')
 canvas.pack()
 
 # Creating individuals
 for i in range(20):
-	newind = Individual()
+    newind = Individual()
 
 # Main loop
-while(True):
+while True:
     Individual.current_generation += 1
-    
+
     for ind in list(Individual.all_individuals.values()):
         ind.walk()
-        
+
     master.update()
     time.sleep(0.015)
 
